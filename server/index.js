097221a6391ef10b5b3907e4b814f3d5a8911239
Akshay krishna M <achuakshay9000@gmail.com>
2020-12-20 14:54:41 +0530

    finished setting up the client

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-
 app.use(express.json());
 // create a connection to the database
 
 require("dotenv").config();
 require("./config/db")();
 require("./config/initDb")();
+
+const PORT = process.env.PORT || 5500;
 
 app.use("/auth", require("./routes/auth"));
 app.use("/departments", require("./routes/department"));
