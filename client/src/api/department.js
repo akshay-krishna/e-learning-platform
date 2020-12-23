ad41feb0a92d3f141c02116d41800e5200e3de38
Akshay krishna M <achuakshay9000@gmail.com>
2020-12-23 15:05:50 +0530

    added some more features

--- client/src/api/department.js
+++ client/src/api/department.js
@@ -13,3 +13,17 @@ export const getAll = async (token) => {
     throw Error("Failed to fetch data");
   }
 };
+
+export const createDepartment = async (token, data) => {
+  try {
+    const res = await axios.post("/departments", data, {
+      headers: {
+        Authorization: token,
+      },
+    });
+    return res.status;
+  } catch (err) {
+    console.error(err.response);
+    throw Error("Failed to create a department");
+  }
+};
--- client/src/components/pages/Department.js
+++ client/src/components/pages/Department.js
@@ -1,13 +1,16 @@
 import { useContext, useEffect, useState } from "react";
-import { getAll } from "../../api/department";
+import { createDepartment, getAll } from "../../api/department";
 import { userContext } from "../../context/userContext";
 import Card from "../layout/Card";
 import "./styles/department.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { useHistory } from "react-router-dom";
 
 const Department = () => {
+  const [newForm, setNewForm] = useState(false);
+
   const { token } = useContext(userContext).user;
   const [departments, setDepartments] = useState([]);
   useEffect(() => {
@@ -20,7 +23,7 @@ const Department = () => {
       }
     };
     fetchData(token);
-  }, [token]);
+  }, [token, newForm]);
   return (
     <div className="department">
       <div className="container">
@@ -30,8 +33,86 @@ const Department = () => {
           })}
         </div>
       </div>
-      <div className="department__addIcon">
-        <FontAwesomeIcon icon={faPlus} size="3x" transform="shrink-4" />
+      <div className="department__addIcon" onClick={() => setNewForm(!newForm)}>
+        <FontAwesomeIcon icon={faPlus} size="3x" transform="shrink-5" />
+      </div>
+      {newForm ? (
+        <NewDepartmentForm newForm={newForm} setNewForm={setNewForm} />
+      ) : null}
+    </div>
+  );
+};
+
+const NewDepartmentForm = ({ newForm, setNewForm }) => {
+  const [department, setDepartment] = useState({ name: "", description: "" });
+  const { token } = useContext(userContext).user;
+  const history = useHistory();
+  const onChange = (e) => {
+    setDepartment({ ...department, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await createDepartment(token, department);
+      setNewForm(!newForm);
+      history.push("/department");
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  const { name, description } = department;
+  return (
+    <div className="department__newDepartment">
+      <div
+        className="department__click"
+        onClick={() => {
+          setNewForm(!newForm);
+        }}
+      ></div>
+      <div className="newDepartment">
+        <form onSubmit={onSubmit}>
+          <div className="newDepartment__title ">
+            <input
+              onChange={onChange}
+              type="text"
+              name="name"
+              value={name}
+              placeholder="department name"
+              onBlur={() => {
+                const ele = document.querySelector(".newDepartment__title");
+                ele.classList.remove("newDepartment--focus");
+              }}
+              onFocus={() => {
+                const ele = document.querySelector(".newDepartment__title");
+                ele.classList.add("newDepartment--focus");
+              }}
+            />
+          </div>
+          <div className="newDepartment__description">
+            <textarea
+              onChange={onChange}
+              onBlur={() => {
+                const ele = document.querySelector(
+                  ".newDepartment__description"
+                );
+                ele.classList.remove("newDepartment--focus");
+              }}
+              onFocus={() => {
+                const ele = document.querySelector(
+                  ".newDepartment__description"
+                );
+                ele.classList.add("newDepartment--focus");
+              }}
+              name="description"
+              value={description}
+              placeholder="Provide a short description"
+              maxLength="580"
+            ></textarea>
+          </div>
+          <button type="submit">Create</button>
+        </form>
       </div>
     </div>
   );
